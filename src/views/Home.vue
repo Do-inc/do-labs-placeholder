1d8c3af57a447fe8ee8c4a0936e4494caf100312
ebalo <e.balsamo000@gmail.com>
2021-07-04 09:31:55 +0200

    favicon, logo and mailchimp integration

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,7 @@
 		<div class="v2_3">
 			<div class="v22_99"></div>
 			<div class="v22_89">
-				<div class="v22_90"></div>
-				<div class="v22_91"></div>
-				<div class="v22_92"></div>
-				<div class="v22_93"></div>
-				<div class="v22_94"></div>
-				<div class="v22_95"></div>
-				<div class="v22_96"></div>
-				<div class="v22_97"></div>
+				<img src="@/assets/images/logo.svg" alt="Do Labs">
 			</div>
 			<span class="v9_2">A new melody’s in the air ...</span>
 			<div class="v22_100">
@@ -18,10 +11,10 @@
 					<div class="v2_64"></div>
 					<span class="v5_75">Subscribe</span>
 				</div>
-				<div class="v9_32">
+				<a class="v9_32" href="mailto:info@doduet.studio">
 					<div class="v7_17"></div>
 					<span class="v7_18">Contact</span>
-				</div>
+				</a>
 			</div>
 		</div>
 	</div>
