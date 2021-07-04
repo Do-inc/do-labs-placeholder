c5c68975eacb1d6a1c81981879ee37d623250359
ebalo <e.balsamo000@gmail.com>
2021-07-04 09:08:02 +0200

    Do labs initial setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="v9_30">
+		<div class="v2_3">
+			<div class="v22_99"></div>
+			<div class="v22_89">
+				<div class="v22_90"></div>
+				<div class="v22_91"></div>
+				<div class="v22_92"></div>
+				<div class="v22_93"></div>
+				<div class="v22_94"></div>
+				<div class="v22_95"></div>
+				<div class="v22_96"></div>
+				<div class="v22_97"></div>
+			</div>
+			<span class="v9_2">A new melody’s in the air ...</span>
+			<div class="v22_100">
+				<div class="v9_31">
+					<div class="v2_64"></div>
+					<span class="v5_75">Subscribe</span>
+				</div>
+				<div class="v9_32">
+					<div class="v7_17"></div>
+					<span class="v7_18">Contact</span>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
